585363a1200789fe5361aeccc49474c8582fd3ea
arman <mdarmanhossen404@gmail.com>
2025-02-09 01:19:55 +0600

    hotel_backend

--- hotels/models.py
+++ hotels/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils.text import slugify
 from django.contrib.auth.models import User
 from django.utils.timezone import now
+import requests
 
 class District(models.Model):
     district_name= models.CharField(max_length=100)
@@ -14,13 +15,13 @@ class District(models.Model):
 
     def __str__(self):
         return self.district_name
-    
+
 
 class Hotel(models.Model):
     hotel_name = models.CharField(max_length=255)
     address = models.TextField()
-    district_names = models.ForeignKey('District',on_delete=models.CASCADE,related_name='hotels',null=True,blank=True)
-    image = models.ImageField(upload_to="hotels/images")
+    district_names = models.ForeignKey('District', on_delete=models.CASCADE, related_name='hotels', null=True, blank=True)
+    image_url = models.CharField(max_length=1024, blank=True)  # Store ImageBB URL here
     description = models.TextField()
     price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
     available_room = models.PositiveIntegerField()
@@ -28,8 +29,37 @@ class Hotel(models.Model):
     def __str__(self):
         return self.hotel_name
 
-    def district_name(self):
-        return self.district_names.district_name if self.district_names else None
+    def upload_image_to_imagebb(self, image_file):
+        api_key = '91eb0ab92307af61f7bffe3b9d728952'
+        url = 'https://api.imgbb.com/1/upload'
+        payload = {
+            'key': api_key,
+            'image': image_file,
+        }
+        response = requests.post(url, data=payload)
+        if response.status_code == 200:
+            return response.json()['data']['url']
+        return None
+
+    def save(self, *args, **kwargs):
+        if self.image_url:  # Check if image is provided
+            self.image_url = self.upload_image_to_imagebb(self.image_url)  # Upload image to ImageBB and get the URL
+        super().save(*args, **kwargs)
+
+# class Hotel(models.Model):
+#     hotel_name = models.CharField(max_length=255)
+#     address = models.TextField()
+#     district_names = models.ForeignKey('District',on_delete=models.CASCADE,related_name='hotels',null=True,blank=True)
+#     image = models.ImageField(upload_to="hotels/images")
+#     description = models.TextField()
+#     price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
+#     available_room = models.PositiveIntegerField()
+
+#     def __str__(self):
+#         return self.hotel_name
+
+#     def district_name(self):
+#         return self.district_names.district_name if self.district_names else None
 
 
 RATING_CHOICES = [
