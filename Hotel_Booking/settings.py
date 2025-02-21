eb5ed7008043e98ee302e42b2c7b299c49806273
arman <mdarmanhossen404@gmail.com>
2025-02-21 20:12:41 +0600

    hotel_backend

--- Hotel_Booking/settings.py
+++ Hotel_Booking/settings.py
@@ -94,17 +94,19 @@ TEMPLATES = [
     },
 ]
 
+
 WSGI_APPLICATION = 'Hotel_Booking.wsgi.app'
+
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:8000',  
-]
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ["https://hotel-backend-3ybx.vercel.app/"]
+CSRF_TRUSTED_ORIGINS = [
+    'https://hotel-backend-arcx.onrender.com',
+]
+
 
 
 DATABASES = {
